7b8a1599bec86f851da7d2a2f25a886ee2c89a5e
Rok Slana <rokslana@gmail.com>
2022-09-12 09:48:07 +0200

    Completed tempConversion.ja

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+//1,8
+//0.5555555556
 
+const ftoc = function(givenTemp) {
+  let convertedTemp = Math.round(((givenTemp - 32) * (5/9))*10)/10; 
+
+  return convertedTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(givenTemp) {
+  let convertedTemp =Math.round(((givenTemp*(9/5) + 32))*10)/10;
+  return convertedTemp;
 };
 
 // Do not edit below this line
