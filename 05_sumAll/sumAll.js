517d72b3b7128a652f7e71971feb6007d6f1bbd4
Rok Slana <rokslana@gmail.com>
2022-09-12 09:01:41 +0200

    Done sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, lastNumber) {
 
+    let finalSum = 0;
+
+    if(firstNumber < 0 || lastNumber<0 || !Number.isInteger(firstNumber) || !Number.isInteger(lastNumber) || !typeof firstNumber =="number" || !typeof lastNumber == "number") {
+        return 'ERROR';
+    }
+
+    else{
+        if(firstNumber>lastNumber){
+            for (let i = firstNumber; i>= lastNumber; i--){
+                finalSum += i;
+            }
+        }else{
+            for (let i = firstNumber; i<= lastNumber; i++){
+                finalSum+=i;
+            }
+        }    
+        return finalSum;
+    }
 };
 
 // Do not edit below this line
