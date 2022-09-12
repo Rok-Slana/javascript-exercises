9e9742df39b10780c4dd47e66f9719dc530d2b47
Rok Slana <rokslana@gmail.com>
2022-09-12 09:28:53 +0200

    Comleted leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,10 @@
-const leapYears = function() {
+const leapYears = function(givenYear) {
+
+    if(givenYear % 4 == 0 && givenYear%100 != 0 || givenYear%400 == 0){
+        return true;
+    }else{
+        return false;
+    }
 
 };
 
